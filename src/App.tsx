78718e1ca5352d76f200326b846a8a0a1cd46578
Vishwajeet singh <vishwajeetsingh262005@gmail.com>
2023-01-23 13:39:07 +0530

    routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-
-import './App.css'
+import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Feed,
+  Navbar,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
-
   return (
-    <div>Hellow world</div>
-  )
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}> </Box>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
